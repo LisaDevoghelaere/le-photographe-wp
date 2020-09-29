cfb2da24a6bbdeab80b18c6677d2cdfc1e1e3347
LisaDevoghelaere <lisa.devoghelaere@gmail.com>
2020-09-29 11:31:53 +0200

    mise en forme

--- wp-content/themes/le-photographe/header.php
+++ wp-content/themes/le-photographe/header.php
@@ -3,34 +3,34 @@
   <head>
       <meta charset="<?php bloginfo('charset'); ?>">
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+      <meta name="description" content="Site de Photographe, ses services, ses actualités et une gallerie d'image">
       
       <?php wp_head(); ?>
   </head>
 
   <body <?php body_class(); ?>>
-  <div class="container">
+  <div class="container-fluid">
     <header class="header">
-        <nav class="navbar navbar-light bg-light">
-          <div>
-            <a href="<?php echo home_url( '/' ); ?>">
-              <img src="<?php echo get_template_directory_uri(); ?>/img/photographe_logo.png" alt="Logo" class="logo">
-            </a>  
-            <h1>Le Photographe</h1> 
+        <div class="upperbar container-fluid">
+          <nav class="barnav container">
+            <div>
+              <a href="<?php echo home_url( '/' ); ?>">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/photographe_logo.png" alt="Logo" class="logo">
+              </a>  
+              <p class="nom-site"><a href="<?php the_permalink() ?>"><?php echo get_bloginfo( 'name' ); ?></a></p> 
+            </div>
+              <?php 
+            if( has_nav_menu('header-menu')){
+              wp_nav_menu(
+                array(
+                'theme_location' => 'header-menu',
+                'menu_class' => 'navbar'
+                )
+              );
+            }
+            ?>
+          
           </div>
-          <?php 
-        if( has_nav_menu('header-menu')){
-          wp_nav_menu(
-            array(
-            'theme_location' => 'header-menu',
-            'menu_class' => 'navbar'
-            )
-          );
-        }
-        ?>
-        </nav>
-        <div class="hero">
-        <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/img/header1.jpg" alt="image du header">
-        </div>
-
+          </nav>
       </header>
       <?php wp_body_open(); ?>
